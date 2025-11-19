bd9ba42b86a07338a8c79704427d6f2ecc8d5f05
GibbedBoomer <mmm04302004@gmail.com>
2025-11-19 11:15:17 -0500

    state machine made

--- code.js
+++ code.js
@@ -1,24 +1,16 @@
 import { Toolbox } from "./toolbox.js";
+import { Game } from "./game.js";
+import { GameOver } from "./gameOver.js";
+import { Title } from "./title.js";
 
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 let toolbox = new Toolbox();
 
-// let myFavoriteLetters = ["a", "b", "c"];
-console.log(toolbox.getRandomColor());
+//make states
+let game = new Game();
+let gameOver = new GameOver();
+let title = new Title();
 
-pencil.fillStyle = toolbox.getRandomColor();
-pencil.fillRect(50, 50, 100, 100);
-
-// let counts = [0,0,0];
-// for(let i = 0; i < 100; i++) {
-// //     let randomIndex = getRandomIndex(myFavoriteLetters);
-// //     counts[randomIndex] += 1;
-
-//     console.log(toolbox.getRandomItem(myFavoriteLetters));
-// }
-
-
-// let shuffled = toolbox.shuffleArray(myFavoriteLetters);
-// console.log(shuffled);
+let state = title;
 
